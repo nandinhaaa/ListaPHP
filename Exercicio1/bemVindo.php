e733e97fd4f6cd25e78d6a2e9087aabe4d22a1c4
Maria Fernanda Gonçalves <91507393+nandinhaaa@users.noreply.github.com>
2024-03-20 21:13:53 -0300

    Update bemVindo.php

--- Exercicio1/bemVindo.php
+++ Exercicio1/bemVindo.php
@@ -4,52 +4,46 @@
   <meta charset="UTF-8">
   <link href="css/login.css" rel="stylesheet" type="text/css" /> 
 </head>
-</script>
 <body>
 <div id="geral">
   <section id="conteudo">
-		  <table>
-			 <tr>
-				<td> 				
-				<?php
-					//obtém os valores digitados no form
-					$valor = $_POST["name"]; //POST É MAIS SEGURO
-					echo "<h4>Bem vindo(a):  $valor </h4>";
-				?>
+    <table>
+      <tr>
+        <td>
+          <?php
+            // Inicializa a variável $username
+            $username = '';
 
-<?php
-// Verifica se os dados do formulário foram enviados
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Simula uma lista de usuários cadastrados no sistema
-    $usuarios_cadastrados = array(
-        "Nanda" => "123",
-        "Amanda" => "123"
-    );
+            // Verifica se os dados do formulário foram enviados
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+              // Obtém o nome de usuário enviado pelo formulário
+              $username = isset($_POST['username']) ? $_POST['username'] : '';
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+              // Simula uma lista de usuários cadastrados no sistema
+              $usuarios_cadastrados = array(
+                "Nanda" => "123",
+                "Amanda" => "123"
+              );
 
-    if (array_key_exists($username, $usuarios_cadastrados) && $usuarios_cadastrados[$username] == $password) {
-        // Usuário autenticado com sucesso
-        echo "<p>Bem-vindo ao sistema</p>";
-    } else {
-        // Usuário não autenticado
-        echo "<p>Você não tem acesso ao sistema</p>";
-    }
-}
-?>
+              $password = isset($_POST['password']) ? $_POST['password'] : '';
 
-
-				</td>
-			 </tr>
-		  </table>	 	
+              // Verifica se o usuário e a senha correspondem aos cadastrados
+              if (array_key_exists($username, $usuarios_cadastrados) && $usuarios_cadastrados[$username] == $password) {
+                // Usuário autenticado com sucesso
+                echo "<p>Bem-vindo ao sistema</p>";
+              } else {
+                // Usuário não autenticado
+                echo "<p>Você não tem acesso ao sistema</p>";
+              }
+            } else {
+              // Caso o formulário não tenha sido enviado
+              echo "<p>Por favor, preencha o formulário</p>";
+            }
+          ?>
+        </td>
+      </tr>
+    </table>   
   </section>
 </div>
-
-
 </body>
 </html>
-
-
-
-
